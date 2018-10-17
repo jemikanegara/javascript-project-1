d73e39f86f4098b570705e8fe3ec9aca66a5b900
jemikanegara <jemikanegara@gmail.com>
2018-10-18 00:43:42 +0800

    add delete function

---
+++ addressbook.js
@@ -0,0 +1,139 @@
+// Registered Citizen       
+people = [{
+    name : 'patrick',
+    address : 'under a stone',
+    email : 'patrickstar@bikinibottom.com',
+    phone : '081-090-090-090'
+}, 
+{
+    name : 'sandy',
+    address : 'under a tree',
+    email : 'sandycheek@bikinibottom.com',
+    phone : '087-080-080-080'
+},
+{
+    name : 'spongebob',
+    address : 'in the pineapple house',
+    email : 'spongebob@bikinibottom.com',
+    phone : '089-209-209-209'
+},
+{
+    name : 'mrcrab',
+    address : 'krusty crab street',
+    email : 'mrcrab@bikinibottom.com',
+    phone : '082-080-909-900'
+}]
+
+// Element
+nameInput = document.getElementById('nameInput')
+addressInput = document.getElementById('addressInput')
+emailInput = document.getElementById('emailInput')
+phoneInput = document.getElementById('phoneInput')
+submitButton = document.querySelector("input[type='submit']")
+error = document.getElementById('error')
+table = document.getElementById('book')
+
+
+// Event Listener
+submitButton.addEventListener('click', register) // add data & show the output
+
+
+// Function
+function output () { 
+    people.forEach( function (element)  {
+    var tr = document.createElement('tr')
+
+    var tdName = document.createElement('td')
+    tdName.className = 'name'
+    var tdAddress = document.createElement('td')
+    tdAddress.className = 'address'
+    var tdEmail = document.createElement('td')
+    tdEmail.className = 'email'
+    var tdPhone = document.createElement('td')
+    tdPhone.className = 'phone'
+    var tdEdit = document.createElement('td')
+    var tdDelete = document.createElement('td')
+
+    var createName = document.createTextNode(element.name)
+    var createAddress = document.createTextNode(element.address)
+    var createEmail = document.createTextNode (element.email)
+    var createPhone = document.createTextNode(element.phone)
+
+    var editBtn = document.createElement('button')
+    var editTxt = document.createTextNode('Edit')
+    editBtn.appendChild(editTxt)
+
+    var deleteBtn = document.createElement('button')
+    var deleteTxt = document.createTextNode('X')
+    deleteBtn.appendChild(deleteTxt)
+    deleteBtn.className = 'delete'
+
+    tdName.appendChild(createName)
+    tdAddress.appendChild(createAddress)
+    tdEmail.appendChild(createEmail)
+    tdPhone.appendChild(createPhone)
+    tdEdit.appendChild(editBtn)
+    tdDelete.appendChild(deleteBtn)
+
+    tr.appendChild(tdName)
+    tr.appendChild(tdAddress)
+    tr.appendChild(tdEmail)
+    tr.appendChild(tdPhone)
+    tr.appendChild(tdEdit)
+    tr.appendChild(tdDelete)
+
+    table.appendChild(tr)
+    })
+
+    deleteButton = document.querySelectorAll('.delete')
+
+    deleteButton.forEach(
+        element => {
+        element.addEventListener('click', deleteData)
+        }
+    )
+}
+
+output () // call the output for the initial page load
+
+function register (event) {
+    event.preventDefault()
+    if (nameInput.value !== "" && addressInput.value !== "" && emailInput.value !== "" && phoneInput.value !== "") {
+    table.innerHTML = ""
+
+    let newPerson = {
+    name : nameInput.value,
+    address : addressInput.value,
+    email : emailInput.value,
+    phone : phoneInput.value
+    }
+
+    people.push(newPerson) // add new citizen to the array of people
+    output() // show new output after new citizen successfully added
+
+    } else {
+    alert('please complete all the field') // alert if there is any input unfulfilled
+    }
+}
+
+function deleteData (e) {
+        if(confirm('Are you sure want to delete this?')) {
+            var tableBody = this.parentNode.parentNode.parentNode
+            var deleteRow = this.parentNode.parentNode
+            var nameValue = deleteRow.firstChild.firstChild.wholeText
+            
+            people.forEach(
+                (element) => {
+                    if(element.name == nameValue){
+                        var index = people.findIndex(
+                            element => element.name === nameValue
+                        )
+                        people.splice(index, 1)
+                    } 
+                }
+            )
+
+
+            tableBody.removeChild(deleteRow)
+        }
+}
--- level27.js
+++ level27.js
@@ -1,16 +1,34 @@
 toDoList = []
 
-button = document.getElementById('todo')
+todoBtn = document.getElementById('todo')
+searchBtn = document.getElementById('search')
 
-button.addEventListener('click', inputToDo)
+todoBtn.addEventListener('click', inputToDo)
+searchBtn.addEventListener('click', searchToDo)
 
 function inputToDo () {
     var todo = prompt('input next to do')
     if (todo != null) {
         toDoList.push(todo)
-        toDoList.forEach(element => {
+        toDoList.forEach((element) => {
             console.log(element)
         });
     }
 
+}
+
+function searchToDo () {
+    var search = prompt('search your task')
+    
+    if (search != '') {
+        for (i=0; i<toDoList.length; i++){
+            if(toDoList[i].includes(search) === true){
+                console.log(`search result : ${toDoList[i]}`)
+            } else {
+                console.log('search result not found')
+            }
+        }
+    } else {
+        alert('input cannot be empty')
+    }
 }
